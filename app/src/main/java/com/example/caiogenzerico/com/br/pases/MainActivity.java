ce57b619c99bb902b9825565706af701a8b6a2e5
caiogenzerico <caiogenzerico@hotmail.com>
2019-02-22 21:08:47 -0300

    Initial commit

---
+++ app/src/main/java/com/example/caiogenzerico/com/br/pases/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.caiogenzerico.com.br.pases;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
